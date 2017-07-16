847542f2cb6e4dd2f6108c327c9ef485fb340405
Albert Stepantsev <admin@pr-of-it.ru>
2017-07-16 16:55:44 +0300

    + Dir::mtime() methods improvements and tests

--- tests/Fs/DirMtimeTest.php
+++ tests/Fs/DirMtimeTest.php
@@ -62,4 +62,49 @@ class DirMtimeTest extends \PHPUnit_Framework_TestCase
         }
     }
 
+    public function testMtimeEmptyDir()
+    {
+        $path = sys_get_temp_dir() . '/FsDirTest_mtime';
+        if (file_exists($path)) {
+            rmdir($path);
+        }
+        $this->assertDirectoryNotExists($path);
+
+        $dir = new Dir($path);
+        mkdir($path);
+        $this->assertTrue($dir->exists());
+        $this->assertEquals(time(), $dir->mtime(), '', 1);
+        $this->assertEquals(time(), $dir->mtime(true, true), '', 1);
+
+        touch($path, time()-1000);
+        $this->assertEquals(time()-1000, $dir->mtime(), '', 1);
+        $this->assertEquals(time()-1000, $dir->mtime(true, true), '', 1);
+
+        rmdir($path);
+    }
+
+    public function testMtimeOnlyDir()
+    {
+        $path = sys_get_temp_dir() . '/FsDirTest_mtime';
+        if (file_exists($path)) {
+            rmdir($path);
+        }
+        $this->assertDirectoryNotExists($path);
+
+        mkdir($path);
+        touch($path . '/test.txt');
+
+        $dir = new Dir($path);
+        $this->assertTrue($dir->exists());
+        $this->assertEquals(time(), $dir->mtime(), '', 1);
+        $this->assertEquals(time(), $dir->mtime(true, true), '', 1);
+
+        touch($path, time()-1000);
+        $this->assertEquals(time(), $dir->mtime(), '', 1);
+        $this->assertEquals(time()-1000, $dir->mtime(true, true), '', 1);
+
+        unlink($path . '/test.txt');
+        rmdir($path);
+    }
+
 }
