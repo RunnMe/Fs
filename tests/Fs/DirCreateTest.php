a5bfd6ed0ae3822e43503faf7b26442ff6ee8501
Albert Stepantsev <admin@pr-of-it.ru>
2017-07-16 17:09:29 +0300

    + Dir::create() methods improvements and tests

--- tests/Fs/DirCreateTest.php
+++ tests/Fs/DirCreateTest.php
@@ -4,6 +4,7 @@ namespace Runn\tests\Fs\Dir;
 
 use Runn\Fs\Dir;
 use Runn\Fs\Exceptions\DirAlreadyExists;
+use Runn\Fs\Exceptions\MkDirError;
 
 class DirCreateTest extends \PHPUnit_Framework_TestCase
 {
@@ -32,6 +33,37 @@ class DirCreateTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->fail();
+    }
+
+    public function testCreateMkDirError()
+    {
+        /** @todo @7.2 PHP_OS_FAMILY  != 'Windows' */
+        if (in_array(PHP_OS, ['WIN32', 'WINNT', 'Windows'])) {
+            return;
+        }
+
+        $dirname = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('FsTest');
+        $this->assertDirectoryNotExists($dirname);
+
+        mkdir($dirname . '/1/2', 0777, true);
+        chmod($dirname . '/1/2', 0000);
+        $this->assertTrue(file_exists($dirname . '/1/2'));
+        $this->assertTrue(is_dir($dirname . '/1/2'));
+        $this->assertFalse(is_writable($dirname . '/1/2'));
+
+        try {
+
+            $dir = new Dir($dirname . '/1/2/3');
+            $dir->create();
+
+        } catch (MkDirError $e) {
+            return;
+        } finally {
+            chmod($dirname . '/1/2', 0777);
+            rmdir($dirname . '/1/2');
+            rmdir($dirname . '/1');
+            rmdir($dirname);
+        }
     }
 
     public function testCreateValid()
