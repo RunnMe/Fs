15f282f5751ed937ed83745c882c8bebfe24524a
JK <yulia.kudryashova@outlook.com>
2018-03-22 10:44:48 +0200

    feature/RUNNME-8: + helper functions tests, operating system determination tests

--- tests/Fs/functionsTest.php
+++ tests/Fs/functionsTest.php
@@ -4,10 +4,29 @@ namespace Runn\tests\Fs\functions;
 
 use function Runn\Fs\canCp;
 use function Runn\Fs\canXcopy;
+use function Runn\Fs\isMacos;
 use function Runn\Fs\isWindows;
 
 class functionsTest extends \PHPUnit_Framework_TestCase
 {
+
+    public function testIsWindows()
+    {
+        if ('\\' === DIRECTORY_SEPARATOR) {
+            $this->assertTrue(isWindows());
+        } else {
+            $this->assertFalse(isWindows());
+        }
+    }
+
+    public function testIsMacos()
+    {
+        if ('/' === DIRECTORY_SEPARATOR && 'Darwin' === trim(shell_exec('uname'))) {
+            $this->assertTrue(isMacos());
+        } else {
+            $this->assertFalse(isMacos());
+        }
+    }
 
     public function testCanCp()
     {
